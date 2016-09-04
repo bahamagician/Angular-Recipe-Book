24f41ef4b56f06aa8bb8b17d130d5ab9853fd7ba
Chris Thronebury <cthronebury@gmail.com>
2016-09-04 16:50:49 -0400

    Add header component and import bootstrap styles

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [BrowserModule, FormsModule],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule {}
